c82fb6a2bba654c8502f69c890c390a4a8024407
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-10-22 15:48:40 +0330

    arash
    add dynamic urls

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from website.views import *
 
+app_name='website'
+
+
 urlpatterns = [
-    path('',index),
+    path('',index,name='index'),
+    path('index',index,name='index')
     
     
 ]
