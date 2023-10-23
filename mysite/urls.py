07a404a5fc852402dc6739542e599d929a583ad1
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-10-23 14:04:11 +0330

    arash
    add functions

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
+    path('blog/', include('blog.urls'))
+    
     
 ]
 
