7b4ca140e9ca7343f9c0f83306a913ecf29e21c4
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-10-28 12:13:25 +0330

    arash
    add some models

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,17 @@ from django.db import models
 
 
 class Post(models.Model):
+    #image
+    #author
     title=models.CharField(max_length=300)
     content=models.TextField()
+    #tag
+    #category
+    counted_view=models.IntegerField(default=0)
+    status=models.BooleanField(default=False)
+    publised_date=models.DateTimeField(null=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+    updated_time=models.DateTimeField(auto_now_add=True)
+    
+
+
