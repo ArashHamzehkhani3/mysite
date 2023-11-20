0481180d31c78059ddc9b6feb4be1f413e424f55
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-11-20 06:18:05 -0800

    arash
    make image field

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Post(models.Model):
-    #image
-    #author
+    image=models.ImageField(upload_to='media/',default='media/default.jpg')
     title=models.CharField(max_length=300)
     content=models.TextField()
     #tag
